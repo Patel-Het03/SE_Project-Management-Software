62b4c4b869de8a57125e586c467dac7cfaa1b33d
Patel-Het03 <hetrpatel03@gmail.com>
2024-07-09 17:26:10 +0530

    Upadate jwt.config.js

--- server/config/jwt.config.js
+++ server/config/jwt.config.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports.authenticate = function(req, res, next){
     try{
-        const decode=jwt.verify(req.cookies.usertoken, "abcdef");
+        const decode=jwt.verify(req.cookies.usertoken, process.env.SECRET_KEY);
         console.log("0000");
         req.userId = decode.id;
         // console.log(req.userId)
