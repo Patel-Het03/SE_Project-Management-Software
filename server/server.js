1725a33be399c571ad846b7d213b40ec59a97350
Patel-Het03 <hetrpatel03@gmail.com>
2024-09-08 17:28:04 +0530

    minor update

--- server/server.js
+++ server/server.js
@@ -8,13 +8,14 @@ require('dotenv').config();
 
 require('../server/config/mongoose.config');
 app.use(cookieParser());
-app.use(
-    cors({
-      // credentials: true,
-      origin: ['http://localhost:3000', 'https://trackerx-frontend.vercel.app','*'],
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
-    })
-  );
+// app.use(
+//     cors({
+//       credentials: true,
+//       origin: ['http://localhost:3000', 'https://trackerx-frontend.vercel.app','*'],
+//       methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
+//     })
+//   );
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 require('./routes/user.routes')(app);
